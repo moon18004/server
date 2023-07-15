c32a3131ec924e6d45697dabdaee5cc880c67a87
YongLeeCode <yonghwan311@gmail.com>
2023-07-15 17:17:19 -0600

    asdf

--- controller/course.js
+++ controller/course.js
@@ -36,33 +36,45 @@ export async function updateCourse(req, res, next){
   const id = req.params.id;
   const {author, subject, code, text} = req.body;
   const body = {author, subject, code, text};
-  const update = await courseRepository.update(id, body);
+  // const update = await courseRepository.update(id, body);
   const course = await courseRepository.getById(id);
+  if(!course){
+    return res.sendStatus(404);
+  }
   if(course.userId !== req.userId){
     return res.sendStatus(403);
   }
+  const updated = await courseRepository.update(id, body);
+  res.status(200).json(updated);
 
-  if(update){
-    res.status(200).json(update);
-  }
-  else{
-    res.status(500).json({message: `Update failed. course ${id} not found`});
-  }
+  // if(update){
+  //   res.status(200).json(update);
+  // }
+  // else{
+  //   res.status(500).json({message: `Update failed. course ${id} not found`});
+  // }
 }
 
 export async function deleteCourse(req, res){
   const id = req.params.id;
-  const courseGetId = await courseRepository.getById(id);
+  const course = await courseRepository.getById(id);
   // const course= await courseRepository.discard(id);
-  
-  if(courseGetId.userId !== req.userId){
+  if(!course){
+    return res.sendStatus(404);
+  }
+  if(course.userId !== req.userId){
     return res.sendStatus(403);
   }
-  if(courseGetId){
-    await courseRepository.discard(id);
-    res.status(200).json(course);
-  }
-  else{
-    res.status(404).json({message: 'No document deleted'});
-  }
+  await courseRepository.discard(id);
+  res.sendStatus(204);
+  // if(courseGetId.userId !== req.userId){
+  //   return res.sendStatus(403);
+  // }
+  // if(courseGetId){
+  //   await courseRepository.discard(id);
+  //   res.status(200).json(course);
+  // }
+  // else{
+  //   res.status(404).json({message: 'No document deleted'});
+  // }
 }
