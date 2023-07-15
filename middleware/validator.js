782e66471451d768ea6d978bf34903b302493153
YongLeeCode <yonghwan311@gmail.com>
2023-07-15 16:28:46 -0600

    vali

--- middleware/validator.js
+++ middleware/validator.js
@@ -1,4 +1,4 @@
-import {validationResult} from 'express-validator';
+import {body, validationResult} from 'express-validator';
 
 
 export const validate = (req, res, next) => {
@@ -9,12 +9,3 @@ export const validate = (req, res, next) => {
   }
   return res.status(400).json({message: error.array()[0].msg});
 }
-
-export const course = [
-  body('author').notEmpty().withMessage('author missing'),
-  body('subject').notEmpty().withMessage('subject missing'),
-  body('code').notEmpty().isEmail().withMessage('code missing'),
-  body('text').notEmpty().withMessage('text is necessary'),
-  
-  validate
-];
--- router/course.js
+++ router/course.js
@@ -1,16 +1,27 @@
 import express from 'express';
 import 'express-async-errors';
 import * as courseController from '../controller/course.js'
-import validation from '../middleware/validator'
+import {body, param, validationResult} from 'express-validator';
+import { validate } from '../middleware/validator.js';
 import { isAuth } from '../middleware/auth.js';
+
+const course = [
+    body('author').notEmpty().withMessage('author missing'),
+    body('subject').notEmpty().withMessage('subject missing'),
+    body('code').notEmpty().isEmail().withMessage('code missing'),
+    body('text').notEmpty().withMessage('text is necessary'),
+    validate
+];
 
 const router = express.Router();
 
 router.get('/', courseController.getCourses);
-router.post('/', validation.course, isAuth, courseController.courseCreate);
+router.post('/', course, isAuth, courseController.courseCreate);
 router.get('/:id', courseController.getCourse);
-router.put('/:id', validation.course, isAuth, courseController.updateCourse);
+router.put('/:id', course, isAuth, courseController.updateCourse);
 router.delete('/:id', isAuth, courseController.deleteCourse);
 
 export default router;
+
+
 
